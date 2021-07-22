e321a3af24f86907dca9113706a8348130ca466d
aditgocendra <gocendra123@gmail.com>
2021-07-22 21:46:00 +0800

    add level menu adventure

---
+++ src/UserInterface/AdventureMenu/AdventureMenu.gd
@@ -0,0 +1,6 @@
+extends Control
+
+
+
+func _on_Level1_pressed():
+	get_tree().change_scene("res://src/Game/Game.tscn")
--- src/UserInterface/MainMenu/MainMenu.gd
+++ src/UserInterface/MainMenu/MainMenu.gd
@@ -4,7 +4,7 @@ extends Control
 
 func _on_Start_pressed():
 # warning-ignore:return_value_discarded
-	get_tree().change_scene("res://src/Game/Game.tscn")
+	get_tree().change_scene("res://src/UserInterface/AdventureMenu/AdventureMenu.tscn")
 
 
 func _on_Quit_pressed():
